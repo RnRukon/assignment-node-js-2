8bf6c072f8e85908a2c2721f5d0d3075e7fe6b46
Your Name <you@example.com>
2022-10-18 08:17:45 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+//middle wares
+app.use(express.json());
+app.use(cors());
+
+
+// route -----------
+// const toursRoute = require('./Routes/Tours.route')
+
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
+});
+
+// post data =======================
+// app.use('/api/v1/tour', toursRoute);
+
+module.exports = app;
